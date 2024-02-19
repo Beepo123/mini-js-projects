13d7a5e1190abe3441f3f07c362c6e625af33f88
your classmate <samuelgipit@gmail.com>
2024-02-19 16:28:38 +0800

    updated js for password generator

--- password-generator/password-generator.js
+++ password-generator/password-generator.js
@@ -1,11 +1,10 @@
 import { generateWeakPass, generateAvgPass, generateStrongPass } from "./utils.js";
 
+let password = '';
 
 document.querySelector(".container").addEventListener("click", (event) => {
   if (event.target.closest(".button")) {
     const passStrength = event.target.classList[0]
-    let password = '';
-
     if(passStrength === 'weak'){
       password = generateWeakPass();
     } else if (passStrength === 'average'){
@@ -18,15 +17,11 @@ document.querySelector(".container").addEventListener("click", (event) => {
   }
 
   if (event.target.closest('.copy-image')){
-    copyToClipboard();
+    console.log(password)
+    navigator.clipboard.writeText(password)
   }
 });
 
 function displayGeneratedPass(pass){
   document.querySelector('input').value = pass;
-}
-
-function copyToClipboard(){
-  const inputValue = document.querySelector('input').value;
-  navigator.clipboard.writeText(inputValue);
 }
